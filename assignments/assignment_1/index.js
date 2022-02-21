9ab27d9e8a6404c90ec5aeb96c5b7d3ae3612af2
satyamk5678 <satyamk56781234@gmail.com>
2022-02-21 21:56:06 +0530

    nodejs ass-1 submitted

--- assignments/assignment_1/index.js
+++ assignments/assignment_1/index.js
@@ -1,12 +1,22 @@
 function getNameFromCommandLine() {
+    let data=process.argv;
+    let name = data[data.length-1]
+    return name
     // Write you code here, name should be taken as args in process.argv
 }
 
 function getNameFromEnv() {
+    process.env.name='Satyam';
+    return (process.env.name);
     // Write your code here
 }
 
 function getNameFromReadLine() {
+    const readline=require('readline');
+    readline.createInterface({
+        input:process.stdin,
+        output: process.stdout
+    })
     // Write your code here
 }
 
